0fe6bb1f2a6097ceb8fc5e0d8ce7d17ddfcedfbe
kmiibfw <kmiibfw@gmail.com>
2025-04-27 16:53:37 +0900

    [add] 回答の更新、一覧の取得

--- front/src/api/voteApi.js
+++ front/src/api/voteApi.js
@@ -1,5 +1,13 @@
 import { sendJsonData } from './apiUtils';
 
+export const updateAnswer = async (formData) => {
+    return sendJsonData('/api/vote/answer', formData, 'put');
+};
+
+export const getAnswerList = async (formData) => {
+    return sendJsonData('/api/vote/question/answer', formData, 'post');
+};
+
 export const registerAnswer = async (formData) => {
     return sendJsonData('/api/vote/answer', formData, 'post');
 };

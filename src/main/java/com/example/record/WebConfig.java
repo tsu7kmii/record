8d5b3ccb59b006e2f31c2d423f988fe0d3f2abcf
kmiibfw <kmiibfw@gmail.com>
2025-04-28 23:33:25 +0900

    [fix] インデント

--- src/main/java/com/example/record/WebConfig.java
+++ src/main/java/com/example/record/WebConfig.java
@@ -8,19 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
-    @Override
-    public void addViewControllers(@NonNull ViewControllerRegistry registry) {
 
-        /**
-         * restAPIとして扱う場合、パスの階層分だけaddViewControllerを設定する必要がある
-         * 
-         */
-        registry.addViewController("/{spring:[^\\.]+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/{spring1:[^\\.]+}/{spring2:[^\\.]+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/{spring1:[^\\.]+}/{spring2:[^\\.]+}/{spring3:[^\\.]+}")
-                .setViewName("forward:/index.html");
-    }
+        @Override
+        public void addViewControllers(@NonNull ViewControllerRegistry registry) {
+
+                /**
+                 * restAPIとして扱う場合、パスの階層分だけaddViewControllerを設定する必要がある
+                 * 
+                 */
+                registry.addViewController("/{spring:[^\\.]+}")
+                        .setViewName("forward:/index.html");
+                registry.addViewController("/{spring1:[^\\.]+}/{spring2:[^\\.]+}")
+                        .setViewName("forward:/index.html");
+                registry.addViewController("/{spring1:[^\\.]+}/{spring2:[^\\.]+}/{spring3:[^\\.]+}")
+                        .setViewName("forward:/index.html");
+        }
     
 }
